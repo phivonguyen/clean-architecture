01c38eb96d20604b3859a02bb73ee812f9d38fc2
phivonguyen <119102251+phivonguyen@users.noreply.github.com>
2024-02-18 19:35:35 +0700

    Commit second

---
+++ BuberDinner.Api/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,42 @@
+using System.Net;
+using System.Text.Json;
+
+namespace BuberDinner.Api.Middleware;
+
+public class ErrorHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ErrorHandlingMiddleware> _logger;
+
+    public ErrorHandlingMiddleware(RequestDelegate next, ILogger<ErrorHandlingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        var code = HttpStatusCode.InternalServerError; // 500 if unexpected
+
+        // if (exception is NotFoundException) code = HttpStatusCode.NotFound;
+        // else if (exception is UnauthorizedException) code = HttpStatusCode.Unauthorized;
+        // else if (exception is BadRequestException) code = HttpStatusCode.BadRequest;
+
+        var result = JsonSerializer.Serialize(new { error = "An error occurred while processing your request" });
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)code;
+        return context.Response.WriteAsync(result);
+    }
+}
